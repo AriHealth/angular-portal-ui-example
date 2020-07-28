72aacac798c31a942226f36ed9bc04bcc052fb4e
Cavero Barca <carlos.cavero@atos.net>
2020-07-29 00:02:47 +0200

    Do the trick to avoid changes in package-lock.json

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -8,4 +8,7 @@ git config user.name "${GITHUB_USERLONGNAME}"
 git remote rm origin
 git remote add origin https://${GITHUB_USERNAME}:${GITHUB_API_TOKEN}@github.com/arihealth/angular-portal-ui-example.git > /dev/null 2>&1
 
+# Do the trick to avoid package-lock.json changes
+git checkout -- .
+
 echo -e "Done\n"
